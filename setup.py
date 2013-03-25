8f58d58b781959edc22a856aedc9f1dcc996be88
Sameer Rahmani <lxsameer@gnu.org>
2013-03-25 19:14:48 +0430

    package skel added

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+#    Djamo - Yetanother Mongodb driver for Django
+#    Copyright (C) 2012-2013 Yellowen
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# -----------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+
+setup(name='Djamo',
+      version='0.1.0',
+      description='Yet another Mongodb driver for Django'
+      author='Sameer Rahmani',
+      author_email='lxsameer@gnu.org',
+      url='http://djamo.yellowen.com/',
+      download_url="http://djamo.yellowen.com/downloads/",
+      keywords="web development mongo",
+      license='GPL v2',
+      packages=find_packages(),
+      install_requires=['django', 'pymongo' ],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Utilities',
+          ]
+)
